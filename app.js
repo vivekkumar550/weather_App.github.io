6a8bc13416d026b4c548a3922b1944101210ddf2
Vivek Kumar <92450181+vivekkumar550@users.noreply.github.com>
2022-01-04 22:59:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,93 @@
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+
+const weatherApi = {
+    key: "a58475c7282a2d1e1c3eec0f759f75e5",
+    baseUrl: "https://api.openweathermap.org/data/2.5/weather",
+}
+
+const searchInputBox = document.getElementById('input-box');
+
+// Event Listener Function on keypress
+searchInputBox.addEventListener('keypress', (event) => {
+
+    if (event.keyCode == 13) {
+        console.log(searchInputBox.value);
+        getWeatherReport(searchInputBox.value);
+        document.querySelector('.weather-body').style.display = "block";
+    }
+
+});
+
+// Get Weather Report
+function getWeatherReport(city) {
+    fetch(`${weatherApi.baseUrl}?q=${city}&appid=${weatherApi.key}&units=metric`)
+        .then(weather => {
+            return weather.json();
+        }).then(showWeatherReport);
+}
+
+// Show Weather Report
+function showWeatherReport(weather) {
+    console.log(weather);
+
+    let city = document.getElementById('city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let temperature = document.getElementById('temp');
+    temperature.innerHTML = `${Math.round(weather.main.temp)}&deg;C`;
+
+    let minMaxTemp = document.getElementById('min-max');
+    minMaxTemp.innerHTML = `${Math.floor(weather.main.temp_min)}&deg;C (min)/ ${Math.ceil(weather.main.temp_max)}&deg;C (max) `;
+
+    let weatherType = document.getElementById('weather');
+    weatherType.innerText = `${weather.weather[0].main}`;
+
+    let date = document.getElementById('date');
+    let todayDate = new Date();
+    date.innerText = dateManage(todayDate);
+
+
+    if (weatherType.textContent == 'Clear') {
+        document.body.style.backgroundImage = "url('https://images.pexels.com/photos/5745683/pexels-photo-5745683.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500')";
+
+    } else if (weatherType.textContent == 'Clouds') {
+
+        document.body.style.backgroundImage = "url('https://images.pexels.com/photos/3888585/pexels-photo-3888585.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500')";
+
+    } else if (weatherType.textContent == 'Haze') {
+
+        document.body.style.backgroundImage = "url('https://images.pexels.com/photos/4406624/pexels-photo-4406624.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500')";
+
+    } else if (weatherType.textContent == 'Rain') {
+
+        document.body.style.backgroundImage = "url('https://images.pexels.com/photos/311039/pexels-photo-311039.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500')";
+
+    } else if (weatherType.textContent == 'Snow') {
+
+        document.body.style.backgroundImage = "url('https://images.pexels.com/photos/7456436/pexels-photo-7456436.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500')";
+
+    } else if (weatherType.textContent == 'Thunderstorm') {
+
+        document.body.style.backgroundImage = "url('https://images.pexels.com/photos/3637060/pexels-photo-3637060.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500')";
+
+    }
+}
+
+// Date manage
+function dateManage(dateArg) {
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    let year = dateArg.getFullYear();
+    let month = months[dateArg.getMonth()];
+    let date = dateArg.getDate();
+    let day = days[dateArg.getDay()];
+
+    return `${date} ${month} (${day}), ${year}`;
+}
+
+
+
+// a58475c7282a2d1e1c3eec0f759f75e5
